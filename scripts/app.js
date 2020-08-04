3cbbd6df50302147c30947a90736f48dfbfe553e
unknown <ramidz130@gmail.com>
2020-08-04 21:05:14 +0100

    finisheed the project: it was all a one day of work - make the nav bar and design the avtice state - make the header - make the plate card and design it - make the side list - make footer

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.list-container .list-group-item').hover(function() {
+        $(this).animate({
+            color: "white",
+            backgroundColor: "#475054"
+        })
+    })
+})
